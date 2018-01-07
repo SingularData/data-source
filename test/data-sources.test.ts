0fe47785fe3f791511a48f5b1a7c7352ed26ebf4
haoliangyu <njkon@foxmail.com>
2018-01-07 14:15:25 -0500

    set up project

---
+++ test/data-sources.test.ts
@@ -0,0 +1,32 @@
+import { expect } from "chai";
+import { readFileSync } from "fs";
+import { join } from "path";
+
+describe("data source", () => {
+  let sources;
+
+  before(() => {
+    sources = JSON.parse(
+      readFileSync(join(__dirname, "/../data-sources.json"), "utf-8")
+    );
+  });
+
+  it("should have name, type, and url.", () => {
+    for (let source of sources) {
+      expect(source.name).to.be.a("string");
+      expect(source.type).to.be.a("string");
+      expect(source.url).to.be.a("string");
+    }
+  });
+
+  it("should have apiKey and apiUrl for Junar sources.", () => {
+    for (let source of sources) {
+      if (source.type.toLowerCase() !== "junar") {
+        continue;
+      }
+
+      expect(source.apiKey).to.be.a("string");
+      expect(source.apiUrl).to.be.a("string");
+    }
+  });
+});
